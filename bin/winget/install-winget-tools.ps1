fd1732040505efc5144198bd8d9d9c83c0266ad8
mjacker <mjackerjaime@gmail.com>
2024-09-01 14:54:03 -0400

    Added my list to lisntall winget apps.

---
+++ bin/winget/install-winget-tools.ps1
@@ -0,0 +1,7 @@
+winget install Git.Git
+winget install GitHub.cli
+winget install 7zip.7zip
+winget install Microsoft.Sysinternals.ZoomIt
+winget install JAMSoftware.TreeSize.Free
+winget install Neovim.Neovim
+winget install JanDeDobbeleer.OhMyPosh
