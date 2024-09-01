ab491f8f95eefe230be889935da5a3bf98941f94
mjacker <mjackerjaime@gmail.com>
2024-09-01 19:53:38 -0400

    Fixed path to windos terminal json.

--- bin/winterminal/edit-windowsterminal-json.ps1
+++ bin/winterminal/edit-windowsterminal-json.ps1
@@ -1,1 +1,1 @@
-nvim C:\Users\mjacker\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+nvim $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
