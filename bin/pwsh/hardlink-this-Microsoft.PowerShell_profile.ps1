d4da2a547909f6a00b16c8de239da9269d4acbbd
mjacker <mjackerjaime@gmail.com>
2024-09-15 10:00:20 -0400

    pwsh hard-link-this-file.

--- bin/pwsh/hardlink-this-Microsoft.PowerShell_profile.ps1
+++ bin/pwsh/hardlink-this-Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,25 @@
 oh-my-posh init pwsh | Invoke-Expression
 Import-Module -Name Terminal-Icons
 
-if ($host.Name -eq 'ConsoleHost')
-{
+$versionMinimum = [Version]'7.1.999'
+
+if ($host.Name -eq 'ConsoleHost') {
 	Import-Module PSReadLine
 }
-Set-PSReadLineOption -PredictionSource History
+if (($host.Name -eq 'ConsoleHost') -and ($PSVersionTable.PSVersion -ge $verisonMinimum)){ Write-Host "Oops"	
+	Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+}
+else {
+	Set-PSReadLineOption -PredictionSource History
+}
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -ShowToolTips
+
+#Autocomplete for Arrow Keys
+# Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+# Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+# Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 Import-Module ~\.mj-dotfiles\bin\pwsh\mjpwsh-profile.ps1
 
