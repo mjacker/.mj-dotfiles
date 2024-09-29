abd5bdc0a76d33c782a5cc6520add82d1e26de35
mjacker-wsl-ubuntu <mjackerjaime@gmail.com>
2024-09-28 23:43:45 -0400

    refactor install dotfiles.

--- install.sh
+++ install.sh
@@ -22,59 +22,79 @@ programs=( \
   "build-essential" \
 	)
 
+installers=( \
+  "nala" \
+  "apt-get" \
+  "pkg" \
+)
 command_exists() {
-	command -v "$1" > /dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
-install_package(){
-	echo "$1"
-	echo "------"
-	if command_exists nala; then
-		echo "Package manager: nala"
-		if command_exists $1; then
-			echo "command $1 installed"
-		else
-			echo "command $1 missing. Installing.."
-			nala install -y "$1"
-		fi
-	elif command_exists apt-get; then
-		echo "Package manager: apt-get:"
-		if command_exists $1; then
-			echo "command $1 installed"
-		else
-			echo "command $1 missing. Installing.."
-			apt-get install -y "$1"
-		fi
-	elif command_exists pkg; then
-		echo "Package manager: apt-get:"
-		if command_exists $1; then
-			echo "command $1 installed"
-		else
-			echo "command $1 missing. Installing.."
-			pkg install -y "$1"
-		fi
-	else
-		echo "Unable to install, packaga not supported."
-		return 1
-	fi
+missing() {
+    printf "\e[31m$1\e[0m\n"
 }
 
-# Check if at least one argument is provided
-if [ $# -gt 0 ]; then
-    if [ "$1" == "--minimal" ] || [ "$1" == "-m" ]; then
-	    for program in "${programs_minimal[@]}"; do
-		install_package $program
-	    done
-    fi
-else
-    if command_exists nala; then
-    	echo "Package manager: nala"
-    	nala update && nala upgrade -y
-    elif command_exists apt-get; then
-	apt-get update && apt-get upgrade -y
-    fi
-    for program in "${programs[@]}"; do
-	install_package $program
-    done
-fi
+info() {
+    printf "$1\n"
+}
 
+warn() {
+    printf "⚠️  \e[33m$1\e[0m\n"
+}
+
+select_installer() {
+  for i in "${installers[@]}"; do
+    if ! command -v $i >/dev/null; then
+        warn "$i not installed!.\n"
+    else
+      installer=$i
+      return 0
+    fi
+  done
+  info "Intaller selected is $installer"
+}
+
+update_installer() {
+  $installer update && $installer upgrade
+}
+
+install_apps(){
+  for program in "${programs[@]}"; do
+    if command_exists $program; then
+      echo "command $program installed."
+    else
+      missing "command $program missing."
+      warn "Installing..."
+      $installer install -y $program
+      if [ $program = "nala" ]; then
+        select_installer
+      fi 
+    fi
+  done
+}
+
+select_installer
+update_installer
+install_apps
+
+
+# # Check if at least one argument is provided
+# if [ $# -gt 0 ]; then
+#     if [ "$1" == "--minimal" ] || [ "$1" == "-m" ]; then
+# 	    for program in "${programs_minimal[@]}"; do
+# 		    install_package $program
+# 	    done
+#     fi
+# else
+#     if command_exists nala; then
+#     	echo "Package manager: nala"
+#     	nala update && nala upgrade -y
+#     elif command_exists apt-get; then
+# 	apt-get update && apt-get upgrade -y
+#     fi
+#     for program in "${programs[@]}"; do
+# 	install_package $program
+#     done
+# fi
+# 
