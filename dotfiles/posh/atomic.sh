4d555e5bce1721c1c03e41e78c01a6c40dc5d9be
mjacker-wsl-ubuntu <mjackerjaime@gmail.com>
2025-04-14 03:44:46 +0530

    Added Atomic theme, ohmyposh

---
+++ dotfiles/posh/atomic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#oh-my-posh init bash --config /home/mjubuntu/.cache/oh-my-posh/themes/kali.omp.json
+
+eval "$(oh-my-posh --init --shell bash --config ~/.cache/oh-my-posh/themes/atomicBit.omp.json)"
