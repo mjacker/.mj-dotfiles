c3dbcaccfeef56dae1c58e519cd089a0c06bd0ce
mjacker <mjackerjaime@gmail.com>
2026-03-09 18:50:42 +0530

    added rule to rc.lua for steam windows.

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -525,6 +525,12 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+    { rule_any = { class = { "Steam", "steam_app" } },
+      properties = {
+      floating = true,
+      fullscreen = true
+      }
+    },
 }
 -- }}}
 
